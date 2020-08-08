f645ea250f945be03da4aba8a81024eb9be43ea2
castro-gabriel <gabrielcastromail@gmail.com>
2020-08-08 01:14:47 -0300

    front-end complete

---
+++ scipts/addFields.js
@@ -0,0 +1,19 @@
+//  Procurar o botão
+document.querySelector('#add-time')
+.addEventListener('click', cloneField)
+//  Quando clicar no botão
+
+//  Executar uma ação
+function cloneField() {
+    //  Duplicar os campos
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true)
+
+    //  Limpar os campos
+    const fields = newFieldContainer.querySelectorAll('input')
+    fields.forEach( (field) => {
+        field.value = ''
+    })
+
+    //  Colocar na página
+    document.querySelector('#schedule-items').append(newFieldContainer)
+}
